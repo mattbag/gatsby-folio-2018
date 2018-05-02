8cdbdf5925a41c11cf1d1f8d4426c3d4d98c849e
mattbag <wised@AirMatt.local>
2018-05-02 20:30:45 +1000

    pretty

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react'
 import styles from './../../pages/master.module.css'
 
-export default class Grid extends Component{
-  constructor(props){
+export default class Grid extends Component {
+  constructor(props) {
     super(props)
-this.cells = 1;
-    this.grid = React.createRef();
+    this.cells = 1
+    this.grid = React.createRef()
   }
-  componentDidMount(){
+  componentDidMount() {
     // console.log(this.grid.current.style)
     // this.grid.current.style.setProoerty('--cells',2)
   }
-  changeCells = ()=>{
-    this.cells > 2 ? this.cells = 1 : this.cells += 1
-    this.grid.current.style.setProperty('--cells',this.cells)
+  changeCells = () => {
+    this.cells > 2 ? (this.cells = 1) : (this.cells += 1)
+    this.grid.current.style.setProperty('--cells', this.cells)
   }
 
   render() {
-    return <div ref={this.grid}
-    className={styles.grid} style={{
-      '--cells':1,
-       gridTemplateColumns: `repeat(var(--cells), 1fr)`
-      }}>
-    <p onClick={this.changeCells} style={{
-      position:'absolute',
-      right: '1rem',
-      top: '2rem',
-      color: '#fff',
-      marginBottom: 0,
-      cursor: 'zoom-out'
-      }}>change</p>
-            {this.props.children}
-            </div>
+    return (
+      <div
+        ref={this.grid}
+        className={styles.grid}
+        style={{
+          '--cells': 1,
+          gridTemplateColumns: `repeat(var(--cells), 1fr)`,
+        }}
+      >
+        <p
+          onClick={this.changeCells}
+          style={{
+            position: 'absolute',
+            right: '1rem',
+            top: '2rem',
+            color: '#fff',
+            marginBottom: 0,
+            cursor: 'zoom-out',
+          }}
+        >
+          change
+        </p>
+        {this.props.children}
+      </div>
+    )
   }
 }
-
-
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,36 @@
-import React from "react"
-import Img from "gatsby-image"
+import React from 'react'
+import Img from 'gatsby-image'
 import Grid from './../components/Grid/index'
 
-import styles from "./master.module.css"
+import styles from './master.module.css'
 // import logo from "../gatsby-light.svg"
 
 export default ({ data }) => (
   <div className={styles.wrap}>
-
     <Grid>
-      {data && data.allSitesYaml.edges.map(({ node }, index) => (
-        <div className={styles.site} key={index}>
-          <a href={node.url} target="_blank">
-          <div className={styles.pic}>
-
-            <Img
-              sizes={node.childScreenshot.screenshotFile.childImageSharp.sizes}
-              alt={node.name}
-              className={styles.shadow}
-              />
-
-            </div>
-            <div className={styles.label}>
-              <div className={styles.label__in}>{node.name}</div>
-            </div>
-          </a>
-        </div>
-      ))}
+      {data &&
+        data.allSitesYaml.edges.map(({ node }, index) => (
+          <div className={styles.site} key={index}>
+            <a href={node.url} target="_blank">
+              <div className={styles.pic}>
+                <Img
+                  sizes={
+                    node.childScreenshot.screenshotFile.childImageSharp.sizes
+                  }
+                  alt={node.name}
+                  className={styles.shadow}
+                />
+              </div>
+              <div className={styles.label}>
+                <div className={styles.label__in}>{node.name}</div>
+              </div>
+            </a>
+          </div>
+        ))}
     </Grid>
 
-    
-   
-{console.warn('add contact here')}
-{console.warn('add build info/credits')}
+    {console.warn('add contact here')}
+    {console.warn('add build info/credits')}
   </div>
 )
 
